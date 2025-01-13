ca518a4224726ef1000ab9a14f720d0742549aa2
Nelll <fj3983@naver.com>
2025-01-13 09:25:51 +0900

    [Feat] Skeleton Set
    스켈레톤 업데이트, 플레이어 캐릭터 메터리얼 수정, 드래곤 프리팹 정상 작동 확인

--- Assets/InfinityPBR - Magic Pig Games/Scripts/LayerField.cs
+++ Assets/InfinityPBR - Magic Pig Games/Scripts/LayerField.cs
@@ -1,3 +1,24 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class LayerField : PropertyAttribute {}
+public class LayerField : PropertyAttribute
+{
+    private int defaultValue;
+    private string label;
+
+    public LayerField(string label)
+    {
+        this.label = label;
+    }
+
+    public LayerField(string label, int defaultValue) : this(label)
+    {
+        this.defaultValue = defaultValue;
+    }
+
+    public string tooltip { get; internal set; }
+    public string name { get; internal set; }
+
+
+}
