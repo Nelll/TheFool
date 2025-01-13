68b36e7b546827ddbaad4780ff7c00feb1f520ca
Nelll <fj3983@naver.com>
2025-01-13 11:30:10 +0900

    [Chore] Fixed Update
    폴더 정리 및 적 캐릭터 프리팹화

---
+++ Assets/_Scripts/Scriptable Object/Status.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Status", menuName = "Scriptable Objects/Status")]
+public class Status : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private string name;
+    [SerializeField] private float health;
+    [SerializeField] private float mentalPoint;
+    [SerializeField] private int attack;
+    [SerializeField] private int defense;
+
+    public GameObject Prefab
+    { get { return prefab; } }
+    public string Name
+    { get { return name; } }
+    public float Health
+    { get { return health; } }
+    public float MentalPoint
+    { get { return mentalPoint; } }
+    public int Attack
+    { get { return attack; } }
+    public int Defense
+    { get { return defense; } }
+}
