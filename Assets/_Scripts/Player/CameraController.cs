802572954bd269f9e98de36d254caec421c1965d
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-01-15 17:34:14 +0900

    [Feat] Camera Settings, Character Animations and Controller
    - 카메라 세팅
    - Character  Animation(Avatar추가 해서 칼 잡는 모션) 추가
    - Character Controller 수정

--- Assets/_Scripts/Player/CameraController.cs
+++ Assets/_Scripts/Player/CameraController.cs
@@ -2,65 +2,104 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform objectTofollow;
+    public Transform objectToFollow;
     public float followSpeed = 10f;
     public float sensitivity = 100f;
     public float clampAngle = 70f;
+    public LayerMask collisionLayers; // 충돌 검사에 사용할 레이어 마스크
 
     private float rotX;
     private float rotY;
 
     public Transform realCamera;
-    public Vector3 dirNormalized;
-    public Vector3 finalDir;
+    private Vector3 dirNormalized;
+    private float finalDistance;
     public float minDistance;
     public float maxDistance;
     public float smoothness = 10f;
-    public float finalDistance;
-
 
     void Start()
     {
-        rotX = transform.localRotation.eulerAngles.x;
-        rotY = transform.localRotation.eulerAngles.y;
-
-        dirNormalized = realCamera.localPosition.normalized;
-        finalDistance = realCamera.localPosition.magnitude;
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Variables();
+        CursorLock(true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rotX += -(Input.GetAxis("Mouse Y")) * sensitivity * Time.deltaTime;
-        rotY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-
-        Quaternion rot = Quaternion.Euler(rotX, rotY, 0);
-        transform.rotation = rot;
+        CameraRotation();
+        CusorLockExit();
     }
 
     void LateUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, objectTofollow.position, followSpeed * Time.deltaTime);
+        FollowTarget();
+        CameraDistance();
+    }
 
-        finalDir = transform.TransformPoint(dirNormalized * maxDistance);
+    private void Variables()
+    {
+        rotX = transform.localRotation.eulerAngles.x;
+        rotY = transform.localRotation.eulerAngles.y;
+        dirNormalized = realCamera.localPosition.normalized;
+        finalDistance = realCamera.localPosition.magnitude;
+    }
 
+    private void CameraRotation()
+    {
+        rotX += -(Input.GetAxis("Mouse Y")) * sensitivity * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion rotation = Quaternion.Euler(rotX, rotY, 0);
+        transform.rotation = rotation;
+    }
+
+    private void FollowTarget()
+    {
+        if (objectToFollow != null)
+        {
+            Vector3 targetPosition = objectToFollow.position;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogWarning("objectToFollow가 설정되지 않았습니다.");
+        }
+    }
+
+    private void CameraDistance()
+    {
+        Vector3 desiredCameraPos = transform.TransformPoint(dirNormalized * maxDistance);
         RaycastHit hit;
 
-        if (Physics.Linecast(transform.position, finalDir, out hit))
+        if (Physics.Linecast(transform.position, desiredCameraPos, out hit, collisionLayers))
         {
+            Debug.Log("충돌");
             finalDistance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
         }
         else
         {
+            Debug.Log("충돌X");
             finalDistance = maxDistance;
         }
-        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormalized * finalDistance, Time.deltaTime * smoothness);
 
-        //if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) return;
+        realCamera.localPosition = Vector3.Lerp(realCamera.localPosition, dirNormalized * finalDistance, Time.deltaTime * smoothness);
+    }
+
+    private void CursorLock(bool isLocked)
+    {
+        Cursor.lockState = isLocked ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !isLocked;
+    }
+
+    private void CusorLockExit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CursorLock(false);
+        }
+        else if (Input.GetMouseButtonDown(0) && Cursor.lockState != CursorLockMode.Locked)
+        {
+            CursorLock(true);
+        }
     }
 }
